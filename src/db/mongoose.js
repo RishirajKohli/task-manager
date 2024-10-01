695a8453559f4f922f06a4f22cef67d1898c5bc2
Rishabh Kohli <kohli.rishiraj@gmail.com>
2024-10-01 22:23:16 +0530

    updated mongoose version

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((err) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB");
+    throw err;
+  });
